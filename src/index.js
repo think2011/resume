5a49888b3770874c5bf2a1a0a1446d5deb9d1e4a
think2011 <452125301.hzplay@gmail.com>
2017-09-19 05:52:53 +0800

    feat ⛳ 主要项目介绍 refactor 🍰 职业规划

--- src/index.js
+++ src/index.js
@@ -6,40 +6,56 @@ require('./adapt.scss')
 let $morelink = document.querySelectorAll('.more-link')
 
 Array.from($morelink).forEach((item) => {
-    item.addEventListener('click', () => {
-        document.querySelector(`.${item.dataset.remove}`).classList.remove(item.dataset.remove)
-        item.parentNode.removeChild(item)
-    })
+  item.addEventListener('click', () => {
+    document.querySelector(`.${item.dataset.remove}`).classList.remove(item.dataset.remove)
+    item.parentNode.removeChild(item)
+  })
 })
 
 // 案例
 createGallery(require('./project-items'), '.page__3 .contents')
 
+document.querySelector('.page__3 .contents').addEventListener('click', (e) => {
+  let elem = e.srcElement
+
+  while (elem) {
+    let target = null
+    if (elem.nodeType === 1 && (target = elem.getAttribute('data-target'))) {
+      document.querySelector('[data-name="' + target + '"]').click()
+      return true
+    }
+
+    elem = elem.parentNode
+  }
+})
+
 
 function createGallery(items, appendToDom) {
-    let result = []
+  let result = []
 
-    items.forEach((item) => {
-        let caption = `<h5>${item.year}年</h5><p>${item.desc}</p>`
-        let imgs    = item.imgs.slice(1).map((item) => `<a data-caption="${caption}" href="${item}"></a>`).join('')
-        let html    = `
-            <li class="gallery">
-            <a data-caption="${caption}" href="${item.imgs[0]}">
+  items.forEach((item) => {
+    let caption = `<h5>${item.year}年</h5><p>${item.desc}</p>`
+    let imgs    = item.imgs.slice(1).map((item) => `<a data-caption="${caption}" href="${item}"></a>`).join('')
+    let html    = `
+      <div class="item min">
+        <div class="preview">
+            <a data-name="${item.name}" data-caption="${caption}" href="${item.imgs[0]}">
                 <div class="img">
                     <img src="${item.sImg}">
                 </div>
                 <h3>${item.name}</h3>
             </a>
             ${imgs}
-            </li>
+        </div>
+      </div>
 `
-        result.push(html)
-    })
+    result.push(html)
+  })
 
-    document.querySelector(appendToDom).innerHTML = `<ul>${result.join('')}</ul>`
-    baguetteBox.run('.gallery', {
-        filter : /.*/i,
-        preload: 6,
-        async  : true
-    })
+  document.querySelector(appendToDom).innerHTML += result.join('')
+  baguetteBox.run('.item .preview', {
+    filter : /.*/i,
+    preload: 6,
+    async  : true
+  })
 }
--- src/project-items.js
+++ src/project-items.js
@@ -1,219 +1,219 @@
 module.exports = [
-    {
-        name: '微信群发扩展应用',
-        year: 2016,
-        desc: '个人项目，扩展了 web 端微信的群发功能，在不破坏原有功能的条件下追加了操作UI，介入内置接口实现稳定发送',
-        sImg: 'http://think2011.qiniudn.com/wx-helper-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/wx-helper-demo.gif',
-            'http://think2011.qiniudn.com/wx-helper-1.png',
-            'http://think2011.qiniudn.com/wx-helper-2.png',
-            'http://think2011.qiniudn.com/wx-helper-3.png',
-        ]
-    },
-    {
-        name: '抽奖游戏编辑器',
-        year: 2016,
-        desc: '让不懂编码或者有设计能力的用户能够定制想要的抽奖游戏活动',
-        sImg: 'http://think2011.qiniudn.com/lottery-creater-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/lottery-creater-1.png',
-            'http://think2011.qiniudn.com/lottery-creater-2.png',
-            'http://think2011.qiniudn.com/lottery-creater-3.png',
-        ]
-    },
-    {
-        name: '趣互动PC端',
-        year: 2016,
-        desc: '提供互动、抽奖类的游戏给卖家推广产品',
-        sImg: 'http://think2011.qiniudn.com/qhd-1-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/qhd-1.png',
-            'http://think2011.qiniudn.com/qhd-1-1.png',
-            'http://think2011.qiniudn.com/qhd-2.png',
-            'http://think2011.qiniudn.com/qhd-3.png',
-            'http://think2011.qiniudn.com/qhd-4.png',
-            'http://think2011.qiniudn.com/qhd-7.png',
-        ]
-    },
-    {
-        name: '袋鼠云-EasyDB',
-        year: 2016,
-        desc: '兼职项目，数据库运维产品，负责大部分前端页面的开发工作，涉及大量图表和代码复用设计',
-        sImg: 'http://think2011.qiniudn.com/easydb-5.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/easydb-2.png',
-            'http://think2011.qiniudn.com/easydb-3.png',
-            'http://think2011.qiniudn.com/easydb-4.png',
-            'http://think2011.qiniudn.com/easydb-5.png',
-        ]
-    },
-    {
-        name: '促销宝',
-        year: 2015,
-        desc: '卖家可以通过该产品做购物促销、打水印、发送优惠劵、生成手机详情、上架商品等功能',
-        sImg: 'http://think2011.qiniudn.com/cxb-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/cxb-1.png',
-            'http://think2011.qiniudn.com/cxb-1-1.png',
-            'http://think2011.qiniudn.com/cxb-1-2.png',
-            'http://think2011.qiniudn.com/cxb-1-3.png',
-            'http://think2011.qiniudn.com/cxb-2.png',
-            'http://think2011.qiniudn.com/cxb-3.png',
-            'http://think2011.qiniudn.com/cxb-4.png',
-        ]
-    },
-    {
-        name: '趣互动移动端',
-        year: 2016,
-        desc: '卖家可以通过该产品创建管理抽奖、互动活动',
-        sImg: 'http://think2011.qiniudn.com/qhd-m-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/qhd-m-1.png',
-            'http://think2011.qiniudn.com/qhd-m-2.png',
-            'http://think2011.qiniudn.com/qhd-m-3.png',
-            'http://think2011.qiniudn.com/qhd-m-4.png',
-            'http://think2011.qiniudn.com/qhd-m-5.png',
-        ]
-    },
-    {
-        name: '趣互动粉丝中心',
-        year: 2017,
-        desc: '卖家的会员可以通过该产品游玩互动、赚取消费积分、优惠等',
-        sImg: 'http://think2011.qiniudn.com/fans-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/fans-1.png',
-            'http://think2011.qiniudn.com/fans-2.png',
-            'http://think2011.qiniudn.com/fans-3.png',
-            'http://think2011.qiniudn.com/fans-4.png',
-            'http://think2011.qiniudn.com/fans-5.png',
-            'http://think2011.qiniudn.com/fans-6.png',
-        ]
-    },
-    {
-        name: '易拉客',
-        year: 2015,
-        desc: '卖家可以通过该产品做购物促销、打水印、发送优惠劵、生成手机详情、上架商品等功能',
-        sImg: 'http://think2011.qiniudn.com/ylk-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/ylk-1.png',
-            'http://think2011.qiniudn.com/ylk-2.png',
-            'http://think2011.qiniudn.com/ylk-3.png',
-            'http://think2011.qiniudn.com/ylk-4.png',
-        ]
-    },
-    {
-        name: '万卡会员营销核心系统',
-        year: 2014,
-        desc: '核心系统，集成了会员、车辆、商城、微信、员工管理。<br />并通过微信、短信、电话、消费产生的服务数据对会员进行服务、关怀、营销等操作。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p3-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p3-1.png',
-            "http://think2011.qiniudn.com/resume2014-p3-2.png",
-            "http://think2011.qiniudn.com/resume2014-p3-3.png",
-            "http://think2011.qiniudn.com/resume2014-p3-4.png",
-            "http://think2011.qiniudn.com/resume2014-p3-5.png",
-            "http://think2011.qiniudn.com/resume2014-p3-6.png",
-            "http://think2011.qiniudn.com/resume2014-p3-7.png",
-            "http://think2011.qiniudn.com/resume2014-p3-8.png",
-            "http://think2011.qiniudn.com/resume2014-p3-9.png",
-            "http://think2011.qiniudn.com/resume2014-p3-10.png",
-            "http://think2011.qiniudn.com/resume2014-p3-11.png",
-            "http://think2011.qiniudn.com/resume2014-p3-12.png",
-            "http://think2011.qiniudn.com/resume2014-p3-13.png",
-            "http://think2011.qiniudn.com/resume2014-p3-14.png",
-            "http://think2011.qiniudn.com/resume2014-p3-15.png",
-            "http://think2011.qiniudn.com/resume2014-p3-16.png",
-            "http://think2011.qiniudn.com/resume2014-p3-17.png",
-            "http://think2011.qiniudn.com/resume2014-p3-18.png",
-            "http://think2011.qiniudn.com/resume2014-p3-19.png",
-            "http://think2011.qiniudn.com/resume2014-p3-20.png",
-            "http://think2011.qiniudn.com/resume2014-p3-21.png",
-            "http://think2011.qiniudn.com/resume2014-p3-22.png",
-            "http://think2011.qiniudn.com/resume2014-p3-23.png",
-            "http://think2011.qiniudn.com/resume2014-p3-24.png",
-            "http://think2011.qiniudn.com/resume2014-p3-25.png"
-        ]
-    },
-    {
-        name: '与深圳国保对接解锁系统',
-        year: 2014,
-        desc: '对核心系统的二次开发，对接深圳国保解锁系统，允许用户自助管理、解锁保险柜、申请维护服务等。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p4-3.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p4-1.png',
-            "http://think2011.qiniudn.com/resume2014-p4-2.png",
-            "http://think2011.qiniudn.com/resume2014-p4-3.png",
-            "http://think2011.qiniudn.com/resume2014-p4-4.png",
-            "http://think2011.qiniudn.com/resume2014-p4-5.png",
-            "http://think2011.qiniudn.com/resume2014-p4-6.png",
-            "http://think2011.qiniudn.com/resume2014-p4-7.png",
-            "http://think2011.qiniudn.com/resume2014-p4-8.png",
-            "http://think2011.qiniudn.com/resume2014-p4-9.png",
-            "http://think2011.qiniudn.com/resume2014-p4-10.png",
-        ]
-    },
-    {
-        name: '会员微信微商城子系统',
-        year: 2014,
-        desc: '允许用户使用话费、余额、赠金、返现自助购买商品。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p5-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p5-1.png',
-            "http://think2011.qiniudn.com/resume2014-p5-2.png",
-            "http://think2011.qiniudn.com/resume2014-p5-3.png",
-            "http://think2011.qiniudn.com/resume2014-p5-4.png",
-            "http://think2011.qiniudn.com/resume2014-p5-5.png",
-            "http://think2011.qiniudn.com/resume2014-p5-6.png",
-        ]
-    },
-    {
-        name: '会员自助服务子系统',
-        year: 2014,
-        desc: '允许用户自助管理资料、兑换优惠券、兑换积分、防伪验证商品等操作。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p2-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p2-1.png',
-            'http://think2011.qiniudn.com/resume2014-p2-2.png',
-            'http://think2011.qiniudn.com/resume2014-p2-3.png',
-        ]
-    },
-    {
-        name: '益车行车务自助办理系统',
-        year: 2014,
-        desc: '允许用户通过手机拍摄照片，自助查询缴纳违章、车船税、年票等。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p1-3.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p1-1.png',
-            'http://think2011.qiniudn.com/resume2014-p1-2.png',
-            'http://think2011.qiniudn.com/resume2014-p1-3.png',
-            'http://think2011.qiniudn.com/resume2014-p1-4.png',
-            'http://think2011.qiniudn.com/resume2014-p1-5.png',
-        ]
-    },
-    {
-        name: '益车行车务客服办理系统',
-        year: 2014,
-        desc: '允许用户自助查询缴纳违章、车船税、年票等。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p7-1.gif?imageMogr2/format/png/thumbnail/200x',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p7-1.gif',
-            'http://think2011.qiniudn.com/resume2014-p7-2.gif',
-        ]
-    },
-    {
-        name: '2014年 其他',
-        year: 2014,
-        desc: '展示部分系统的设计图、文档等，均在讨论后独立完成。',
-        sImg: 'http://think2011.qiniudn.com/resume2014-p6-1.png?imageView2/2/w/300',
-        imgs: [
-            'http://think2011.qiniudn.com/resume2014-p6-1.png',
-            'http://think2011.qiniudn.com/resume2014-p6-2.png',
-            'http://think2011.qiniudn.com/resume2014-p6-3.png',
-            'http://think2011.qiniudn.com/resume2014-p6-4.png',
-            'http://think2011.qiniudn.com/resume2014-p6-5.png',
-            'http://think2011.qiniudn.com/resume2014-p6-6.png',
-            'http://think2011.qiniudn.com/resume2014-p6-7.png',
-        ]
-    }
+  {
+    name: '趣互动PC端',
+    year: 2016,
+    desc: '提供互动、抽奖类的游戏给卖家推广产品',
+    sImg: 'http://think2011.qiniudn.com/qhd-1-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/qhd-1.png',
+      'http://think2011.qiniudn.com/qhd-1-1.png',
+      'http://think2011.qiniudn.com/qhd-2.png',
+      'http://think2011.qiniudn.com/qhd-3.png',
+      'http://think2011.qiniudn.com/qhd-4.png',
+      'http://think2011.qiniudn.com/qhd-7.png',
+    ]
+  },
+  {
+    name: '趣互动移动端',
+    year: 2016,
+    desc: '卖家可以通过该产品创建管理抽奖、互动活动',
+    sImg: 'http://think2011.qiniudn.com/qhd-m-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/qhd-m-1.png',
+      'http://think2011.qiniudn.com/qhd-m-2.png',
+      'http://think2011.qiniudn.com/qhd-m-3.png',
+      'http://think2011.qiniudn.com/qhd-m-4.png',
+      'http://think2011.qiniudn.com/qhd-m-5.png',
+    ]
+  },
+  {
+    name: '微信群发扩展应用',
+    year: 2016,
+    desc: '个人项目，扩展了 web 端微信的群发功能，在不破坏原有功能的条件下追加了操作UI，介入内置接口实现稳定发送',
+    sImg: 'http://think2011.qiniudn.com/wx-helper-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/wx-helper-demo.gif',
+      'http://think2011.qiniudn.com/wx-helper-1.png',
+      'http://think2011.qiniudn.com/wx-helper-2.png',
+      'http://think2011.qiniudn.com/wx-helper-3.png',
+    ]
+  },
+  {
+    name: '抽奖游戏编辑器',
+    year: 2016,
+    desc: '让不懂编码或者有设计能力的用户能够定制想要的抽奖游戏活动',
+    sImg: 'http://think2011.qiniudn.com/lottery-creater-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/lottery-creater-1.png',
+      'http://think2011.qiniudn.com/lottery-creater-2.png',
+      'http://think2011.qiniudn.com/lottery-creater-3.png',
+    ]
+  },
+  {
+    name: '袋鼠云-EasyDB',
+    year: 2016,
+    desc: '和远程团队合作的项目，数据库运维产品，负责大部分前端页面的开发工作，涉及大量图表和代码复用设计',
+    sImg: 'http://think2011.qiniudn.com/easydb-5.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/easydb-2.png',
+      'http://think2011.qiniudn.com/easydb-3.png',
+      'http://think2011.qiniudn.com/easydb-4.png',
+      'http://think2011.qiniudn.com/easydb-5.png',
+    ]
+  },
+  {
+    name: '促销宝',
+    year: 2015,
+    desc: '卖家可以通过该产品做购物促销、打水印、发送优惠劵、生成手机详情、上架商品等功能',
+    sImg: 'http://think2011.qiniudn.com/cxb-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/cxb-1.png',
+      'http://think2011.qiniudn.com/cxb-1-1.png',
+      'http://think2011.qiniudn.com/cxb-1-2.png',
+      'http://think2011.qiniudn.com/cxb-1-3.png',
+      'http://think2011.qiniudn.com/cxb-2.png',
+      'http://think2011.qiniudn.com/cxb-3.png',
+      'http://think2011.qiniudn.com/cxb-4.png',
+    ]
+  },
+  {
+    name: '趣互动粉丝中心',
+    year: 2017,
+    desc: '卖家的会员可以通过该产品游玩互动、赚取消费积分、优惠等',
+    sImg: 'http://think2011.qiniudn.com/fans-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/fans-1.png',
+      'http://think2011.qiniudn.com/fans-2.png',
+      'http://think2011.qiniudn.com/fans-3.png',
+      'http://think2011.qiniudn.com/fans-4.png',
+      'http://think2011.qiniudn.com/fans-5.png',
+      'http://think2011.qiniudn.com/fans-6.png',
+    ]
+  },
+  {
+    name: '易拉客',
+    year: 2015,
+    desc: '卖家可以通过该产品做购物促销、打水印、发送优惠劵、生成手机详情、上架商品等功能',
+    sImg: 'http://think2011.qiniudn.com/ylk-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/ylk-1.png',
+      'http://think2011.qiniudn.com/ylk-2.png',
+      'http://think2011.qiniudn.com/ylk-3.png',
+      'http://think2011.qiniudn.com/ylk-4.png',
+    ]
+  },
+  {
+    name: '万卡会员营销核心系统',
+    year: 2014,
+    desc: '核心系统，集成了会员、车辆、商城、微信、员工管理。<br />并通过微信、短信、电话、消费产生的服务数据对会员进行服务、关怀、营销等操作。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p3-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p3-1.png',
+      "http://think2011.qiniudn.com/resume2014-p3-2.png",
+      "http://think2011.qiniudn.com/resume2014-p3-3.png",
+      "http://think2011.qiniudn.com/resume2014-p3-4.png",
+      "http://think2011.qiniudn.com/resume2014-p3-5.png",
+      "http://think2011.qiniudn.com/resume2014-p3-6.png",
+      "http://think2011.qiniudn.com/resume2014-p3-7.png",
+      "http://think2011.qiniudn.com/resume2014-p3-8.png",
+      "http://think2011.qiniudn.com/resume2014-p3-9.png",
+      "http://think2011.qiniudn.com/resume2014-p3-10.png",
+      "http://think2011.qiniudn.com/resume2014-p3-11.png",
+      "http://think2011.qiniudn.com/resume2014-p3-12.png",
+      "http://think2011.qiniudn.com/resume2014-p3-13.png",
+      "http://think2011.qiniudn.com/resume2014-p3-14.png",
+      "http://think2011.qiniudn.com/resume2014-p3-15.png",
+      "http://think2011.qiniudn.com/resume2014-p3-16.png",
+      "http://think2011.qiniudn.com/resume2014-p3-17.png",
+      "http://think2011.qiniudn.com/resume2014-p3-18.png",
+      "http://think2011.qiniudn.com/resume2014-p3-19.png",
+      "http://think2011.qiniudn.com/resume2014-p3-20.png",
+      "http://think2011.qiniudn.com/resume2014-p3-21.png",
+      "http://think2011.qiniudn.com/resume2014-p3-22.png",
+      "http://think2011.qiniudn.com/resume2014-p3-23.png",
+      "http://think2011.qiniudn.com/resume2014-p3-24.png",
+      "http://think2011.qiniudn.com/resume2014-p3-25.png"
+    ]
+  },
+  {
+    name: '与深圳国保对接解锁系统',
+    year: 2014,
+    desc: '对核心系统的二次开发，对接深圳国保解锁系统，允许用户自助管理、解锁保险柜、申请维护服务等。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p4-3.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p4-1.png',
+      "http://think2011.qiniudn.com/resume2014-p4-2.png",
+      "http://think2011.qiniudn.com/resume2014-p4-3.png",
+      "http://think2011.qiniudn.com/resume2014-p4-4.png",
+      "http://think2011.qiniudn.com/resume2014-p4-5.png",
+      "http://think2011.qiniudn.com/resume2014-p4-6.png",
+      "http://think2011.qiniudn.com/resume2014-p4-7.png",
+      "http://think2011.qiniudn.com/resume2014-p4-8.png",
+      "http://think2011.qiniudn.com/resume2014-p4-9.png",
+      "http://think2011.qiniudn.com/resume2014-p4-10.png",
+    ]
+  },
+  {
+    name: '会员微信微商城子系统',
+    year: 2014,
+    desc: '允许用户使用话费、余额、赠金、返现自助购买商品。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p5-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p5-1.png',
+      "http://think2011.qiniudn.com/resume2014-p5-2.png",
+      "http://think2011.qiniudn.com/resume2014-p5-3.png",
+      "http://think2011.qiniudn.com/resume2014-p5-4.png",
+      "http://think2011.qiniudn.com/resume2014-p5-5.png",
+      "http://think2011.qiniudn.com/resume2014-p5-6.png",
+    ]
+  },
+  {
+    name: '会员自助服务子系统',
+    year: 2014,
+    desc: '允许用户自助管理资料、兑换优惠券、兑换积分、防伪验证商品等操作。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p2-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p2-1.png',
+      'http://think2011.qiniudn.com/resume2014-p2-2.png',
+      'http://think2011.qiniudn.com/resume2014-p2-3.png',
+    ]
+  },
+  {
+    name: '益车行车务自助办理系统',
+    year: 2014,
+    desc: '允许用户通过手机拍摄照片，自助查询缴纳违章、车船税、年票等。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p1-3.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p1-1.png',
+      'http://think2011.qiniudn.com/resume2014-p1-2.png',
+      'http://think2011.qiniudn.com/resume2014-p1-3.png',
+      'http://think2011.qiniudn.com/resume2014-p1-4.png',
+      'http://think2011.qiniudn.com/resume2014-p1-5.png',
+    ]
+  },
+  {
+    name: '益车行车务客服办理系统',
+    year: 2014,
+    desc: '允许用户自助查询缴纳违章、车船税、年票等。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p7-1.gif?imageMogr2/format/png/thumbnail/200x',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p7-1.gif',
+      'http://think2011.qiniudn.com/resume2014-p7-2.gif',
+    ]
+  },
+  {
+    name: '2014年 其他',
+    year: 2014,
+    desc: '展示部分系统的设计图、文档等，均在讨论后独立完成。',
+    sImg: 'http://think2011.qiniudn.com/resume2014-p6-1.png?imageView2/2/w/300',
+    imgs: [
+      'http://think2011.qiniudn.com/resume2014-p6-1.png',
+      'http://think2011.qiniudn.com/resume2014-p6-2.png',
+      'http://think2011.qiniudn.com/resume2014-p6-3.png',
+      'http://think2011.qiniudn.com/resume2014-p6-4.png',
+      'http://think2011.qiniudn.com/resume2014-p6-5.png',
+      'http://think2011.qiniudn.com/resume2014-p6-6.png',
+      'http://think2011.qiniudn.com/resume2014-p6-7.png',
+    ]
+  }
 ]
